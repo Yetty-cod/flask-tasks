05bd232121912342deb901a91a1177ee3f7b2d7d
Yetty-cod <klisak.e@yandex.ru>
2024-02-24 17:23:12 +0300

    Первая задача. Добавлена главная страница с выбором заголовка

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def main_page(title):
+    '''Главная страница'''
+    return render_template('base.html', title=title)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
