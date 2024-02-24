84d0483ac2f4ec03d0c0e5b72741f27ee47a323c
Yetty-cod <klisak.e@yandex.ru>
2024-02-24 17:26:36 +0300

    Вторая задача. Добавлена страница с выбором профессии и отображения нужного места на корабле

--- main.py
+++ main.py
@@ -11,5 +11,11 @@ def main_page(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def profession(prof):
+    '''Страница с профессией'''
+    return render_template('index.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
