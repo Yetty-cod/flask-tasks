82d9072b079ea31112852b7188a7471b74a379fc
Yetty-cod <klisak.e@yandex.ru>
2024-02-25 11:15:53 +0300

    Четвёртая задача. Добавлена страница с отображением заполненной формы

--- main.py
+++ main.py
@@ -19,10 +19,21 @@ def profession(prof):
 
 @app.route('/list_prof/<list>')
 def prof_list(list):
+    '''Страница со списком профессий'''
     prof = ['Инструктор по туризму', 'Бортпроводник', 'Сотрудник круизного лайнера', 'Парфюмер',
             'Модель', 'Шоколатье', 'Уборщик мест преступлений', 'Психолог домашних животных',
             'Частный сыровар', 'Пилот воздушного шара']
     return render_template('prof_list.html', prof=prof, list=list)
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    '''Страница с заполненной анкетой'''
+    fields = {'Name': 'Vasya', 'Surname': 'Pupkin', 'Age': 147, 'Education': 'Not bad, not good',
+              'Motivation': 'I love red:/', 'Sex': 'Male', 'Profession': 'Engineer',
+              'Ready to stay on Mars': 'I\'ll think..'}
+    return render_template('auto_answer.html', fields=fields)
 
 
 if __name__ == '__main__':
