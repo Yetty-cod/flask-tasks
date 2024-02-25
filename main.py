8dc1b811c5cd2a2c4724a46ea82b7880d56948f5
Yetty-cod <klisak.e@yandex.ru>
2024-02-25 12:38:20 +0300

    Пятая задача. На картинку убил времени больше чем на форму

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    '''Форма для принятия важных решений'''
+    id1 = StringField('id астронавта', validators=[DataRequired()])
+    pas1 = PasswordField('Пароль астронавта', validators=[DataRequired()])
+
+    id2 = StringField('id капитана', validators=[DataRequired()])
+    pas2 = PasswordField('Пароль капитана', validators=[DataRequired()])
+
+    submit = SubmitField('Войти')
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/<title>')
@@ -34,6 +49,15 @@ def answer():
               'Motivation': 'I love red:/', 'Sex': 'Male', 'Profession': 'Engineer',
               'Ready to stay on Mars': 'I\'ll think..'}
     return render_template('auto_answer.html', fields=fields)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    '''Страница для принятия авжных решений'''
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('form.html', form=form)
 
 
 if __name__ == '__main__':
